2546d39f1f4245a0b39996ed6cc6bb4a3219ce71
Ana <lukas@baginski.mobi>
2023-03-08 16:27:06 +0100

    Create CopyAppMac.sh

---
+++ CopyAppMac.sh
@@ -0,0 +1,9 @@
+rm -f /Volumes/Untitled/@MainMem/PROGRAM/COOKING1.g3m
+rm -f /Volumes/Untitled/@MainMem/PROGRAM/COOKING1.txt
+
+cp COOKING1$1.txt /Volumes/Untitled/@MainMem/PROGRAM/COOKING1.txt
+
+umount /Volumes/Untitled
+
+echo "Copied application successfully."
+exit 0
